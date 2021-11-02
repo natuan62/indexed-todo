5db21ba0075bfbe808b6949a787bed337eb10345
Tuan Nguyen <tuan@rowboatsoftware.com>
2021-11-02 10:39:42 +0700

    prepare for practice

--- src/composables/useIndexedDB.ts
+++ src/composables/useIndexedDB.ts
@@ -9,24 +9,27 @@ const useIndexedDB = (table: string) => {
       if (database.value) {
         resolve(database.value);
       }
-      const request = window.indexedDB.open('todos', 1);
-      request.onerror = (event) => {
-        reject({ msg: 'ERROR: Unable to open database', event });
-      };
-      request.onsuccess = (event: any) => {
-        database.value = event.target.result;
-        resolve(database.value);
-      };
-      request.onupgradeneeded = (event: any) => {
-        const db = event.target.result;
-        let objectStore;
-        if (!db.objectStoreNames.contains('users')) {
-          objectStore = db.createObjectStore('users', {
-            autoIncrement: true,
-            keyPath: 'id',
-          });
-        }
-      };
+      //--- START CODE
+
+      //--- END CODE
+    });
+  };
+
+  const get = async (): Promise<Todo[]> => {
+    database.value = await connect();
+    return new Promise((resolve, reject) => {
+      //--- START CODE
+
+      //--- END CODE
+    });
+  };
+
+  const create = (data: Todo) => {
+    const rawData = toRaw(data);
+    return new Promise((resolve, reject) => {
+      //--- START CODE
+      
+      //--- END CODE
     });
   };
 
@@ -43,31 +46,6 @@ const useIndexedDB = (table: string) => {
     });
   };
 
-  const get = async (): Promise<Todo[]> => {
-    database.value = await connect();
-    return new Promise((resolve, reject) => {
-      const transaction = database.value.transaction('users', 'readonly');
-      const store = transaction.objectStore('users');
-      const result: Todo[] = [];
-      store.openCursor().onsuccess = (event: any) => {
-        const cursor = event.target.result;
-        console.log('get() onsuccess', cursor);
-        if (cursor) {
-          result.push(cursor.value);
-          cursor.continue();
-        }
-      };
-      transaction.oncomplete = () => {
-        console.log('get() oncomplete', result);
-        resolve(result);
-      };
-      transaction.onerror = () => {
-        console.log('get() error');
-        reject([]);
-      };
-    });
-  };
-  
   const getById = async (id: number): Promise<Todo> => {
     database.value = await connect();
     return new Promise((resolve, reject) => {
@@ -81,21 +59,6 @@ const useIndexedDB = (table: string) => {
       request.onerror = () => {
         console.log('getById() error');
         reject({});
-      };
-    });
-  };
-
-  const create = (data: Todo) => {
-    const rawData = toRaw(data);
-    return new Promise((resolve, reject) => {
-      const request = database.value.transaction(['users'], 'readwrite').objectStore('users').add(rawData);
-      request.onsuccess = (event: any) => {
-        const result = { ...rawData, id: event.target.result };
-        console.log('create() onsuccess', event.target.result, result);
-        resolve(result);
-      };
-      request.onerror = () => {
-        console.log('create() onerror');
       };
     });
   };
